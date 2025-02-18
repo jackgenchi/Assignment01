7041052d9d190b1fc457f7e859edc9302370244b
Jack Genchi <jackgenchi@gmail.com>
2025-02-17 22:57:30 -0800

    allStepsCompleted

--- root/js/script.js
+++ root/js/script.js
@@ -33,21 +33,93 @@ let boolContactUser = window.prompt("May we contact you?");*/
 
 
 // STEP 6
-let jsCoercion = "tada" + 10;
-alert(jsCoercion);
+/*let jsCoercion = "tada " + 10;
+alert(jsCoercion); */
+
 // STEP 7
+/*let jsBoolString = "True " + false;
+alert(jsBoolString);
+let jsBoolInt = 10 + true;
+alert(jsBoolInt); */
+
 // STEP 8
+//Original: invalid. does not escape the quotation marks and therefore creates compiler errors 
+/*let someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"'
+console.log(someString)*/
+//corrected: escapes each of the relevant single and double quotation marks
+/*let someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"'
+console.log(someString)*/
+
 // STEP 9
+/*//Null value
+let nullValue = null;
+alert(nullValue);
+//Undefined value
+let undefinedVariable;
+alert(undefinedVariable);*/
+
 // STEP 10
+/*alert(typeof(15));
+alert(typeof("howdy"));
+alert(typeof(true));
+let unaryTestObject;
+alert(typeof(unaryTestObject));
+alert(typeof(null));*/
+
 // STEP 11
+//alert("Hello " + "Jack Genchi" + ", welcome to the JavaScript class");
+
 // STEP 12
+/*let name = "Jack Genchi";
+alert("Hello " + name +", welcome to the JavaScript class");*/
+
 // STEP 13
+/*let course = "JavaScript";
+let name = "Jack Genchi";
+alert("Hello " + name +", welcome to the " + course + " class");*/
+
 // STEP 14
+/*let course = "JavaScript";
+let name = "Jack Genchi";
+alert("Hello " + name +".\nWelcome to the " + course + " class");*/
+
 // STEP 15
+/*let course = "JavaScript";
+let name = prompt("Please enter your name: ");
+alert("Hello " + name +".\nWelcome to the " + course + " class");*/
+
 // STEP 16
+/*let course = prompt("Please enter the name of the course you are taking: ");
+let name = prompt("Please enter your name: ");
+alert("Hello " + name +".\nWelcome to the " + course + " class");*/
+
 // STEP 17
+/*let x = 10;
+let y = 20;
+alert(x+y);*/
+
 // STEP 18
+/*let x = 20;
+x += x;
+alert(x);*/
+
 // STEP 19
+/*let x = 20;
+x *= 5;
+alert(x);*/
+
 // STEP 20
+/*let x = 20%3;
+x /= 1;
+alert(x);*/
+
 // STEP 21
+/*let x = 10;
+let y = 5;
+let z = 2;
+alert(x/y>=z && x%y==0);*/
+
 // STEP 22
+/*let i = 81;
+let j = 9;
+alert(i==j || i<=j);*/
