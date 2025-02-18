2ab0a8096594781278a14c1859ceace81b426501
Jack Genchi <jackgenchi@gmail.com>
2025-02-17 19:49:45 -0800

    steps1-6

---
+++ root/js/script.js
@@ -0,0 +1,53 @@
+// STEP 1
+/*let someMonth;
+function theMonth();
+let currentMonth;
+let summerMonth;
+let myLibraryFunction; */
+
+// STEP 2
+/*//numeric literal expression:
+2.5
+//string literal expression:
+'Hello World'
+//boolean literal expression:
+true
+//null literal expression:
+null */
+
+// STEP 3
+/*var taxRate = 7.75;
+let userInput = prompt('What is your name?');*/
+
+// STEP 4
+/*//camelCase named immutable variables
+const firstName, lastName, address, city;
+//hungarian notation mutable variables
+let strState, intZipCode, intAge, strReferralSource, boolContactUser;*/
+
+// STEP 5 
+/*const firstName = 'Jack';
+let strState;
+strState = 'CA';
+let boolContactUser = window.prompt("May we contact you?");*/
+
+
+// STEP 6
+let jsCoercion = "tada" + 10;
+alert(jsCoercion);
+// STEP 7
+// STEP 8
+// STEP 9
+// STEP 10
+// STEP 11
+// STEP 12
+// STEP 13
+// STEP 14
+// STEP 15
+// STEP 16
+// STEP 17
+// STEP 18
+// STEP 19
+// STEP 20
+// STEP 21
+// STEP 22
